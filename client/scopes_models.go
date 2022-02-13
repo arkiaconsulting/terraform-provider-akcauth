3e8353bfde3d3e89f03c02cbc5569bc5e7e876e3
Louis <louis@arkiaconsulting.com>
2022-02-13 16:46:06 +0100

    Start rework Api model

--- client/scopes_models.go
+++ client/scopes_models.go
@@ -1,9 +1,24 @@
 package client
 
 type ApiScopeCreate struct {
-	Name string `json:"name"`
+	DisplayName             string            `json:"displayName"`
+	Description             string            `json:"description"`
+	ShowInDiscoveryDocument bool              `json:"showInDiscoveryDocument"`
+	UserClaims              []string          `json:"userClaims"`
+	Properties              map[string]string `json:"properties"`
+	Enabled                 bool              `json:"enabled"`
+	Required                bool              `json:"required"`
+	Emphasize               bool              `json:"emphasize"`
 }
 
 type ApiScope struct {
-	Name string `json:"name"`
+	Name                    string            `json:"name"`
+	DisplayName             string            `json:"displayName"`
+	Description             string            `json:"description"`
+	ShowInDiscoveryDocument bool              `json:"showInDiscoveryDocument"`
+	UserClaims              []string          `json:"userClaims"`
+	Properties              map[string]string `json:"properties"`
+	Enabled                 bool              `json:"enabled"`
+	Required                bool              `json:"required"`
+	Emphasize               bool              `json:"emphasize"`
 }
